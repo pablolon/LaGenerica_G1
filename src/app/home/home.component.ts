603524b8f24f8841cfa18487faf8437c4e7884e4
pablolon <90942692+pablolon@users.noreply.github.com>
2021-11-16 22:46:58 -0500

    LOGIN sin plantilla
    Login para desarrollo completo sin plantilla
    
    Co-Authored-By: jesusLayton <86248063+jesusLayton@users.noreply.github.com>
    Co-Authored-By: carjimgu19 <90942804+carjimgu19@users.noreply.github.com>
    Co-Authored-By: dannapardo <91998598+dannapardo@users.noreply.github.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,5 +11,30 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  title = 'Login';
+  
+  user_correcto:string="admininicial";
+  pass_correcto:string="admin123456";
 
+  user:string="";
+  pass:string="";
+
+  correcto:number=-1;
+
+  // si queremos más usuarios
+  lista_users =["diego","carjim","admin","user"];
+
+  comparar(){
+    if(this.user===this.user_correcto){
+      this.correcto=1;
+      if(this.pass===this.pass_correcto){
+        this.correcto=1;
+      }else{
+        this.correcto=0;
+      }
+    }else{
+      this.correcto=0;
+    }
+  }
 }
