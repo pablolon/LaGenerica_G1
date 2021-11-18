985cc50745278150d50f1b79d1e65336c750c2cb
pablolon <90942692+pablolon@users.noreply.github.com>
2021-11-17 21:11:31 -0500

    Avance General

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  title = 'Login';
+  
+  user_correcto:string="admininicial";
+  pass_correcto:string="admin123456";
+
+  user:string="";
+  pass:string="";
+
+  correcto:number=-1;
+
+  // si queremos más usuarios
+  lista_users =["diego","carjim","admin","user"];
+
+  comparar(){
+    if(this.user===this.user_correcto){
+      this.correcto=1;
+      if(this.pass===this.pass_correcto){
+        this.correcto=1;
+      }else{
+        this.correcto=0;
+      }
+    }else{
+      this.correcto=0;
+    }
+  }
+}
